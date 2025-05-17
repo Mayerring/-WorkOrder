0352e899d8593f3bca91ee29801ad1c3761fe224
raoying.chen <1602197209@qq.com>
2025-05-17 21:03:32 +0800

    初始化提交

---
+++ backend/src/main/java/com/example/spring_vue_demo/SpringVueDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_vue_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVueDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVueDemoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/spring_vue_demo/SpringVueDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_vue_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVueDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
