8c87492a1f0809056d3dd257e32fc1b8e128e933
wangdayu <1466840216@qq.com>
2025-06-02 21:35:31 +0800

    根据id查询员工信息

---
+++ backend/src/main/java/com/example/spring_vue_demo/controller/User/UserController.java
@@ -0,0 +1,23 @@
+package com.example.spring_vue_demo.controller.User;
+
+import com.example.spring_vue_demo.entity.Result;
+import com.example.spring_vue_demo.service.UserService;
+import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+    @ApiOperationSupport(order = 3)
+    @Operation(summary = "根据查询员工的信息")
+    @PostMapping("belong")
+    public Result belong(Long staffId) {
+        return userService.belong(staffId);
+    }
+}
