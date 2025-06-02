56343aec8ee1e20d8f5af3f3059d0316e912d836
wangdayu <1466840216@qq.com>
2025-06-02 14:10:38 +0800

    实现登录功能，返回token

--- backend/src/main/resources/workorder.sql
+++ backend/src/main/resources/workorder.sql
@@ -2,56 +2,99 @@ create database wos;
 use wos;
 drop table if exists work_order;
 create table work_order(
-    id bigint primary key NOT NULL auto_increment comment 'id',
-    code varchar(64) NOT NULL comment '编号',
-    type tinyint not null  comment '类型',
-    title varchar(128) comment '工单标题',
-    priority_level tinyint comment '优先级',
-    status int not null comment '状态',
-    create_time bigint not null comment '创建时间',
-    update_time bigint comment '更新时间',
-    cancel_time  bigint comment '取消时间',
-    delete_time  bigint comment '删除时间',
-    content text comment '详情',
-    accessory_url varchar(128) comment '附件url',
-    accessory_name varchar(128) comment '附件名称',
-    deleted tinyint not null comment '删除位'
+                           id bigint primary key NOT NULL auto_increment comment 'id',
+                           code varchar(64) NOT NULL comment '编号',
+                           type tinyint not null  comment '类型',
+                           title varchar(128) comment '工单标题',
+                           priority_level tinyint comment '优先级',
+                           status int not null comment '状态',
+                           create_time bigint not null comment '创建时间',
+                           update_time bigint comment '更新时间',
+                           cancel_time  bigint comment '取消时间',
+                           delete_time  bigint comment '删除时间',
+                           content text comment '详情',
+                           accessory_url varchar(128) comment '附件url',
+                           accessory_name varchar(128) comment '附件名称',
+                           deleted tinyint not null comment '删除位'
 )comment '工单';
 
 drop table if exists handle_log;
 create table handle_log(
-    id bigint primary key NOT NULL auto_increment comment 'id',
-    order_id bigint NOT NULL comment '工单id',
-    content text not null comment '内容',
-    create_time bigint comment '创建时间'
+                           id bigint primary key NOT NULL auto_increment comment 'id',
+                           order_id bigint NOT NULL comment '工单id',
+                           content text not null comment '内容',
+                           create_time bigint comment '创建时间'
 ) comment '操作日志';
 
 drop table if exists message;
 create table message(
-   id bigint primary key NOT NULL auto_increment comment 'id',
-   receiver_id bigint NOT NULL comment '接收人id',
-   sender_id bigint NOT NULL comment '发送人id',
-   type tinyint NOT NULL comment '类型',
-   content text not null comment '内容',
-   send_time bigint comment '发送时间'
+                        id bigint primary key NOT NULL auto_increment comment 'id',
+                        receiver_id bigint NOT NULL comment '接收人id',
+                        sender_id bigint NOT NULL comment '发送人id',
+                        type tinyint NOT NULL comment '类型',
+                        content text not null comment '内容',
+                        send_time bigint comment '发送时间'
 ) comment '消息';
 
 Drop table handle_user_info;
 create table handle_user_info(
-    id bigint primary key NOT NULL auto_increment comment 'id',
-    order_id bigint NOT NULL comment '工单id',
-    user_id bigint NOT NULL COMMENT '用户id',
-    user_name varchar(64) NOT NULL COMMENT '用户名',
-    handle_type tinyint NOT NULL COMMENT '用户类型（1：提交人，2：审核人，3：派单人，4：处理人，5：确认人）',
-    company_id bigint NOT NULL COMMENT '公司id',
-    company_name varchar(128) NOT NULL COMMENT '公司名',
-    department_id bigint NOT NULL COMMENT '部门id',
-    department_name varchar(128) NOT NULL COMMENT '部门名',
-    handle_time bigint comment '处理时间',
-    create_time bigint NOT NULL comment '创建时间',
-    update_time bigint comment '修改时间',
-    remark varchar(255) comment '备注'
+                     id bigint primary key NOT NULL auto_increment comment 'id',
+                     order_id bigint NOT NULL comment '工单id',
+                     user_id bigint NOT NULL COMMENT '用户id',
+                     user_name varchar(64) NOT NULL COMMENT '用户名',
+                     handle_type tinyint NOT NULL COMMENT '用户类型（1：提交人，2：审核人，3：派单人，4：处理人，5：确认人）',
+                     company_id bigint NOT NULL COMMENT '公司id',
+                     company_name varchar(128) NOT NULL COMMENT '公司名',
+                     department_id bigint NOT NULL COMMENT '部门id',
+                     department_name varchar(128) NOT NULL COMMENT '部门名',
+                     handle_time bigint comment '处理时间',
+                     create_time bigint NOT NULL comment '创建时间',
+                     update_time bigint comment '修改时间',
+                     remark varchar(255) comment '备注'
 )comment '工单操作信息';
 
+drop table if exists company;
+CREATE TABLE company (
+                         id BIGINT PRIMARY KEY AUTO_INCREMENT COMMENT '公司ID',
+                         name VARCHAR(100) NOT NULL UNIQUE COMMENT '公司名称',
+                         code VARCHAR(50) UNIQUE COMMENT '公司编码',
+                         parent_company_od BIGINT DEFAULT NULL COMMENT '上级公司ID',
+                         level TINYINT DEFAULT 1 COMMENT '公司层级（1：总部，2：省公司，3：市公司）',
+                         create_time DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                         update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间'
+) COMMENT='公司信息表';
+
+drop table if exists department;
+CREATE TABLE department (
+                            id BIGINT PRIMARY KEY AUTO_INCREMENT COMMENT '部门ID',
+                            name VARCHAR(100) NOT NULL COMMENT '部门名称',
+                            code VARCHAR(50) UNIQUE COMMENT '部门编码',
+                            parent_department_id BIGINT DEFAULT NULL COMMENT '上级部门ID(null表示顶级）',
+                            company_id BIGINT NOT NULL COMMENT '所属公司ID',
+                            leader_id BIGINT DEFAULT NULL COMMENT '部门主管员工ID',
+                            create_time DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                            update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间'
+) COMMENT='部门信息表';
+
+
+drop table if exists staff;
+CREATE TABLE staff (
+                       id BIGINT PRIMARY KEY AUTO_INCREMENT COMMENT '主键ID',
+                       staff_number VARCHAR(50) NOT NULL COMMENT '工号',
+                       password VARCHAR(100) NOT NULL COMMENT '密码（加密）',
+                       name VARCHAR(100) NOT NULL COMMENT '姓名',
+                       company_code VARCHAR(50) NOT NULL COMMENT '所属公司id',
+                       company VARCHAR(100) COMMENT '所属公司',
+                       department_code VARCHAR(50) COMMENT '部门id',
+                       department VARCHAR(100) COMMENT '部门',
+                       position VARCHAR(100) COMMENT '职位',
+                       status TINYINT DEFAULT 0 COMMENT '状态：0 正常，1 休假，2 停职，3 离职',
+                       manager_number BIGINT COMMENT '直属领导工号',
+                       manager_name VARCHAR(100) COMMENT '直属领导姓名',
+                       phone VARCHAR(20) COMMENT '手机号码',
+                       email VARCHAR(100) COMMENT '邮箱',
+                       create_time DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                       update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间'
+) COMMENT='职工表';
 
 
